f4a587aadc51cbaa9af99bcff3a12d7889775acd
Nadine Randel <nadine@voodoo-arts.net>
2023-11-21 11:45:58 +0000

    extract avg px value at mask

--- dff-extraction.py
+++ dff-extraction.py
@@ -1,6 +1,6 @@
 # %%
-
 # Import libraries
+import os
 import numpy as np
 import tifffile as tf
 import pandas as pd
@@ -8,7 +8,7 @@ from scipy.spatial.distance import cdist
 
 #%%
 
-# Load the TIFF stack
+# Load one TIFF stack to check dimension
 tiff_stack = tf.imread('/Users/nadine/Documents/Zlatic_lab/manual_registration_1099/dff_WB/test_made-up_stack/t_1001.tiff')
 
 # Check the dimensions (shape) of the TIFF stack
@@ -18,6 +18,8 @@ print("Dimensions (shape) of TIFF stack:", tiff_stack.shape) # z,y,x in px
 print("Data type:", tiff_stack.dtype)
 print("Number of images in stack:", tiff_stack.shape[0])  # Assuming 1st dimension represents the number of images
 print("Image size (height x width):", tiff_stack.shape[1], "x", tiff_stack.shape[2])
+
+"tiff_stack.shape[0] = z, tiff_stack.shape[1] = y, and tiff_stack.shape[2]) = x"
 
 # %%
 
@@ -56,69 +58,53 @@ coordinates_data['LM_z'] = (coordinates_data['LM_z'] / px_depth).astype(int)
 #print(coordinates_data["LM_z"])
 
 # %%
+# Iterate through each tiff stack
 
-# Iterate through each TIFF file in the folder
-for filename in tif_files:
+folder_path = '/Users/nadine/Documents/Zlatic_lab/manual_registration_1099/dff_WB/test_made-up_stack'  # Replace with your folder path
+tiff_files = [f for f in os.listdir(folder_path) if f.endswith('.tif') or f.endswith('.tiff')]
+
+for filename in tiff_files:
     file_path = os.path.join(folder_path, filename)
     image = tf.imread(file_path)
 
-    # Iterate through each set of coordinates for each image
     for index, row in coordinates_data.iterrows():
         x, y, z = row['LM_x'], row['LM_y'], row['LM_z']
-        radius = 2  # Define the radius of the sphere
+        radius_x = 2  # Radius for x
+        radius_y = 2  # Radius for y
+        radius_z = 1  # Radius for z
 
-        # Create a grid of coordinates that cover the sphere
-        LM_x_range = np.arange(0, image.shape[1])
-        LM_y_range = np.arange(0, image.shape[2])
-        LM_z_range = np.arange(0, image.shape[0])
+        x_range = np.arange(0, image.shape[2])  # x-axis
+        y_range = np.arange(0, image.shape[1])  # y-axis
+        z_range = np.arange(0, image.shape[0])  # z-axis
 
-        xx, yy, zz = np.meshgrid(LM_x_range, LM_y_range, LM_z_range)
-        sphere_mask = ((xx - x) ** 2 + (yy - y) ** 2 + (zz - z) ** 2) <= radius ** 2
+        xx, yy, zz = np.meshgrid(x_range, y_range, z_range, indexing='ij')
 
-        # Apply the mask to the current 3D image to extract the pixels within the sphere
+        # Generate sphere masks for each axis
+        sphere_mask_x = (xx - x) ** 2 <= radius_x ** 2
+        sphere_mask_y = (yy - y) ** 2 <= radius_y ** 2
+        sphere_mask_z = (zz - z) ** 2 <= radius_z ** 2
+
+        # Combine masks for x, y, and z axes
+        #sphere_mask = sphere_mask_x & sphere_mask_y & sphere_mask_z
+
+        # Combine masks for x, y, and z axes
+        sphere_mask = sphere_mask_x & sphere_mask_y & sphere_mask_z
+
+        # Transpose sphere_mask dimensions to match the image stack's shape
+        sphere_mask = np.transpose(sphere_mask, axes=(2, 1, 0))
+
+        # Check sphere mask properties
+        #print("Sphere Mask Shape:", sphere_mask.shape)
+        #print("Number of True Values in Mask:", np.sum(sphere_mask))
+
+        # Apply mask to image
         pixels_in_sphere = image[sphere_mask]
-        print(pixels_in_sphere)
+        #print("Pixels in Sphere:", pixels_in_sphere)
 
-        # Calculate the average pixel intensity within the sphere
+        # Calculate average pixel intensity within the sphere
         average_intensity = np.mean(pixels_in_sphere)
 
         print(f"Average intensity for coordinates ({x}, {y}, {z}) "
               f"in image {filename}: {average_intensity}")
-
-
-# %%
-
-#TEST (coordinates xyz vs zyx) does not work pixel empty 
-
-
-
-# Iterate through each TIFF file in the folder
-for filename in tif_files:
-    file_path = os.path.join(folder_path, filename)
-    image = tf.imread(file_path)
-
-    # Iterate through each set of coordinates for each image
-    for index, row in coordinates_data.iterrows():
-        x, y, z = row['LM_x'], row['LM_y'], row['LM_z']
-        radius = 2  # Define the radius of the sphere
-
-        # Create a grid of coordinates that cover the sphere
-        x_range = np.arange(0, image.shape[1])
-        y_range = np.arange(0, image.shape[2])
-        z_range = np.arange(0, image.shape[0])
-
-        xx, yy, zz = np.meshgrid(z_range, y_range, x_range)
-        sphere_mask = ((xx - z) ** 2 + (yy - y) ** 2 + (zz - x) ** 2) <= radius ** 2
-
-        # Apply the mask to the current 3D image to extract the pixels within the sphere
-        pixels_in_sphere = image[sphere_mask]
-        print(pixels_in_sphere)
-
-        # Calculate the average pixel intensity within the sphere
-        average_intensity = np.mean(pixels_in_sphere)
-
-        print(f"Average intensity for coordinates ({x}, {y}, {z}) "
-              f"in image {filename}: {average_intensity}")
-
 
 # %%
