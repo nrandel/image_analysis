5cbfdb3db3602ca442d92fec829be1fa4b256066
Nadine Randel <nadine@voodoo-arts.net>
2023-11-24 14:05:37 +0000

    append csv numerical

--- behaviour_extraction.py
+++ behaviour_extraction.py
@@ -2,6 +2,45 @@
 # Import libraries
 import numpy as np
 import pandas as pd
+import os
+import re
+
+# %%
+
+# Path to the directory containing your CSV files
+directory_path = '/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces'
+
+# List all CSV files in the directory
+csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
+
+# Sort CSV files if necessary (change the sorting logic as needed)
+#csv_files.sort()  # Change this sorting logic based on your requirement (sorts, 1, 10, 11, 2, 3,..)
+
+# Create a sorted copy of the list of CSV file names with a custom key for numerical sorting
+def extract_number(filename):
+    try:
+        return int(re.search(r'\d+', filename.split('_')[-1]).group())
+    except AttributeError:
+        return float('inf')  # Return a high value for filenames where a number isn't found
+
+sorted_csv_files = sorted(csv_files, key=extract_number)
+
+# Print the sorted list of CSV file names
+for file in sorted_csv_files:
+    print(file)
+
+# Initialize an empty DataFrame to store the merged data
+merged_data = pd.DataFrame()
+
+# Iterate through each sorted CSV file, read it, and append its contents to the merged_data DataFrame
+for file in sorted_csv_files:
+    file_path = os.path.join(directory_path, file)
+    data = pd.read_csv(file_path)
+    merged_data = pd.concat([merged_data, data], ignore_index=True)
+
+# Output the merged data to a single CSV file
+merged_data.to_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/merged_activity_traces.csv', index=False)
+
 
 # %%
 # Load csvfile
@@ -19,10 +58,10 @@ desired_event = 'F'
 columns_to_keep = ["start", "end", "beh"]
 
 # Filter rows where 'beh' column matches the desired event type
-filtered_behavioural_data = behavioural_data[behavioural_data['beh'] == desired_event][columns_to_keep]
+filtered_behavioural_data_single_event = behavioural_data[behavioural_data['beh'] == desired_event][columns_to_keep]
 
 # Show the filtered data
-print(filtered_behavioural_data)
+print(filtered_behavioural_data_single_event)
 
 # %%
 # Define multiple events for 'AND' condition
@@ -68,14 +107,38 @@ behavioural_data['delay'] = behavioural_data['beh_after_start'] - behavioural_da
 columns_to_keep = ["start", "end", "beh", "beh_after", "beh_after_start"]
 
 # Filter rows where preceding event is e.g., 'F' and postceding event occurs after a delay
-filtered_behavioural_data = behavioural_data[(behavioural_data['beh'] == preceding_event) & (behavioural_data['beh_after'].isin(postceding_event)) & (behavioural_data['delay'] <= max_delay_frames)]
+filtered_behavioural_data_sequence = behavioural_data[(behavioural_data['beh'] == preceding_event) & (behavioural_data['beh_after'].isin(postceding_event)) & (behavioural_data['delay'] <= max_delay_frames)]
 
 # Show the filtered data
-print(filtered_behavioural_data)
+print(filtered_behavioural_data_sequence)
 
 # Test 
 #print(behavioural_data[behavioural_data['beh'] == preceding_event])  # Check rows where 'beh' matches the preceding event
 #print(behavioural_data[behavioural_data['beh_after'].isin(postceding_event)])  # Check rows where 'beh_after' matches postceding events
 #print(behavioural_data[behavioural_data['delay'] == delay_frames])  # Check rows where delay matches delay_frames
 
+# %%
+# TEST
+
+import os
+import re
+
+# Path to the directory containing your CSV files
+directory_path = '/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces'
+
+# List all CSV files in the directory
+csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
+
+# Create a sorted copy of the list of CSV file names with a custom key for numerical sorting
+def extract_number(filename):
+    try:
+        return int(re.search(r'\d+', filename.split('_')[-1]).group())
+    except AttributeError:
+        return float('inf')  # Return a high value for filenames where a number isn't found
+
+sorted_csv_files = sorted(csv_files, key=extract_number)
+
+# Print the sorted list of CSV file names
+for file in sorted_csv_files:
+    print(file)
 # %%
