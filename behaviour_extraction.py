2d852995eb092c5095572b1e581345ef9c6a4000
Nadine Randel <nadine@voodoo-arts.net>
2023-11-24 14:39:58 +0000

    fix sequence events

--- behaviour_extraction.py
+++ behaviour_extraction.py
@@ -6,7 +6,8 @@ import os
 import re
 
 # %%
-
+'''
+# Generate merged csv of activity traces
 # Path to the directory containing your CSV files
 directory_path = '/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces'
 
@@ -40,10 +41,10 @@ for file in sorted_csv_files:
 
 # Output the merged data to a single CSV file
 merged_data.to_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/merged_activity_traces.csv', index=False)
-
+'''
 
 # %%
-# Load csvfile
+# Load csvfile of behavioural events used for analysis (model)
 
 behavioural_data = pd.read_csv('/Users/nadine/Documents/paper/single-larva/BW_included_event_annotations.csv')
 
@@ -95,50 +96,28 @@ print(filtered_behavioural_data_or)
 preceding_event = 'TR'
 
 # Define the postceding event type 
-postceding_event = ['B']  # You can define multiple postceding events e.g., ['TL', 'TR']
+postceding_event = ['B']
 
 # Define the maximum allowable delay in frames
-max_delay_frames = 6  # Change this value according to your requirement
+max_delay_frames = 6
 
 # Calculate the delay between beh_end and beh_after_start
 behavioural_data['delay'] = behavioural_data['beh_after_start'] - behavioural_data['end']
 
 # Define columns to keep
-columns_to_keep = ["start", "end", "beh", "beh_after", "beh_after_start"]
+columns_to_keep = ['start', 'end', 'beh', 'beh_after', 'beh_after_start']
 
-# Filter rows where preceding event is e.g., 'F' and postceding event occurs after a delay
-filtered_behavioural_data_sequence = behavioural_data[(behavioural_data['beh'] == preceding_event) & (behavioural_data['beh_after'].isin(postceding_event)) & (behavioural_data['delay'] <= max_delay_frames)]
+# Filter rows based on conditions
+filtered_data = behavioural_data[
+    (behavioural_data['beh'] == preceding_event) &
+    (behavioural_data['beh_after'].isin(postceding_event)) &
+    (behavioural_data['delay'] <= max_delay_frames)
+]
+
+# Select specific columns from the filtered rows
+filtered_behavioural_data_sequence = filtered_data[columns_to_keep]
 
 # Show the filtered data
 print(filtered_behavioural_data_sequence)
 
-# Test 
-#print(behavioural_data[behavioural_data['beh'] == preceding_event])  # Check rows where 'beh' matches the preceding event
-#print(behavioural_data[behavioural_data['beh_after'].isin(postceding_event)])  # Check rows where 'beh_after' matches postceding events
-#print(behavioural_data[behavioural_data['delay'] == delay_frames])  # Check rows where delay matches delay_frames
-
-# %%
-# TEST
-
-import os
-import re
-
-# Path to the directory containing your CSV files
-directory_path = '/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces'
-
-# List all CSV files in the directory
-csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
-
-# Create a sorted copy of the list of CSV file names with a custom key for numerical sorting
-def extract_number(filename):
-    try:
-        return int(re.search(r'\d+', filename.split('_')[-1]).group())
-    except AttributeError:
-        return float('inf')  # Return a high value for filenames where a number isn't found
-
-sorted_csv_files = sorted(csv_files, key=extract_number)
-
-# Print the sorted list of CSV file names
-for file in sorted_csv_files:
-    print(file)
 # %%
