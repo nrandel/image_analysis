7668c0a005ca020d46760bfe8c5f4349d2f152c1
Nadine Randel <nadine@voodoo-arts.net>
2023-11-24 19:36:33 +0000

    add mean

--- plots/activity_traces.py
+++ plots/activity_traces.py
@@ -8,8 +8,8 @@ final_output = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated
 
 # %%
 
-# Group by Event_Number and calculate the maximum time point for each event
-event_time_range_max = final_output.groupby('Event_Number')['Time_Point'].max() 1
+# Group by Event_Number and reset time point for each event
+event_time_range_max = final_output.groupby('Event_Number')['Time_Point'].max() -1
 event_time_range_min = final_output.groupby('Event_Number')['Time_Point'].min() -1
 
 event_time_range = event_time_range_max - event_time_range_min
@@ -17,8 +17,8 @@ event_time_range = event_time_range_max - event_time_range_min
 # Define the neurons you want to include in the plot
 neurons_to_plot = [89409]  # Replace with the specific neuron IDs you want to include, e.g., [89409, 48991]
 
-# Plot values per event over reset time points for selected neurons
-# ... (Previous code remains the same)
+# %%
+# Plot values per event over time points for selected neurons
 
 plt.figure(figsize=(10, 6))  # Adjust figure size if needed
 for neuron_id in neurons_to_plot:
@@ -42,6 +42,115 @@ for neuron_id in neurons_to_plot:
     plt.legend()
 
 plt.tight_layout()
+plt.show()
+
+# %%
+# TEST for mean value 
+
+
+
+
+
+
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file into a DataFrame
+final_output = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/final_output.csv')
+
+# Group by Event_Number and reset time point for each event
+event_time_range_max = final_output.groupby('Event_Number')['Time_Point'].max() - 1
+event_time_range_min = final_output.groupby('Event_Number')['Time_Point'].min() - 1
+event_time_range = event_time_range_max - event_time_range_min
+
+# Define the neurons you want to include in the plot
+neurons_to_plot = [89409]  # Replace with the specific neuron IDs you want to include, e.g., [89409, 48991]
+
+# Dictionary to store values for each time point
+time_point_values = {}
+
+# Iterate through neurons
+for neuron_id in neurons_to_plot:
+    for event_label, event_data in final_output[final_output['Neuron'] == neuron_id].groupby('Event_Number'):
+        time_point_range = event_time_range.loc[event_label] + 1  # Fetch time point range for the event (+1 for inclusive range)
+        values = event_data[event_data['Event_Number'] == event_label]['Value'].values[:time_point_range]  # Select values corresponding to the time point range
+        
+        # Aggregate values per time point
+        for i, val in enumerate(values):
+            if i not in time_point_values:
+                time_point_values[i] = []
+            time_point_values[i].append(val)
+
+# Calculate mean values per time point
+mean_values_per_time_point = [sum(vals) / len(vals) for vals in time_point_values.values()]
+
+# Plot mean values per time point
+plt.figure(figsize=(8, 6))  # Adjust figure size if needed
+plt.plot(range(len(mean_values_per_time_point)), mean_values_per_time_point, label='Mean', linestyle='--', color='black')
+
+plt.xlabel('Time Points')
+plt.ylabel('Mean Value')
+plt.title('Mean Value Across All Events per Time Point')
+plt.legend()
+plt.show()
+
+
+# %%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file into a DataFrame
+final_output = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/final_output.csv')
+
+
+# Group by Event_Number and reset time point for each event
+event_time_range_max = final_output.groupby('Event_Number')['Time_Point'].max() - 1
+event_time_range_min = final_output.groupby('Event_Number')['Time_Point'].min() - 1
+event_time_range = event_time_range_max - event_time_range_min
+
+# Define the neurons you want to include in the plot
+neurons_to_plot = [89409]  # Replace with the specific neuron IDs you want to include, e.g., [89409, 48991]
+
+# Dictionary to store values for each time point
+time_point_values = {}
+
+# Iterate through neurons
+for neuron_id in neurons_to_plot:
+    for event_label, event_data in final_output[final_output['Neuron'] == neuron_id].groupby('Event_Number'):
+        time_point_range = event_time_range.loc[event_label] + 1  # Fetch time point range for the event (+1 for inclusive range)
+        values = event_data[event_data['Event_Number'] == event_label]['Value'].values[:time_point_range]  # Select values corresponding to the time point range
+        
+        # Aggregate values per time point
+        for i, val in enumerate(values):
+            if i not in time_point_values:
+                time_point_values[i] = []
+            time_point_values[i].append(val)
+
+# Calculate mean values per time point
+mean_values_per_time_point = [sum(vals) / len(vals) for vals in time_point_values.values()]
+
+# Plot mean values per time point and individual lines
+plt.figure(figsize=(8, 6))  # Adjust figure size if needed
+
+# Plot individual lines with transparency
+for neuron_id in neurons_to_plot:
+    for event_label, event_data in final_output[final_output['Neuron'] == neuron_id].groupby('Event_Number'):
+        time_point_range = event_time_range.loc[event_label] + 1
+        values = event_data[event_data['Event_Number'] == event_label]['Value'].values[:time_point_range]
+
+        plt.plot(range(time_point_range), values, label=f'Event {event_label}', alpha=0.5)  # Set alpha for transparency
+
+# Plot mean values as a solid black line
+plt.plot(range(len(mean_values_per_time_point)), mean_values_per_time_point, label='Mean', linestyle='-', color='black')
+
+plt.xlabel('Time Points')
+plt.ylabel('Value')
+plt.title('Individual Lines (Transparent) and Mean (Solid) per Time Point')
+plt.legend()
 plt.show()
 
 # %%
