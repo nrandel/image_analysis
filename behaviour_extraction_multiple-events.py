dbe878f31981fb3e01a486be3c2ce5ba3834c131
Nadine Randel <nadine@voodoo-arts.net>
2024-02-13 19:13:18 +0000

    minor

--- behaviour_extraction_multiple-events.py
+++ behaviour_extraction_multiple-events.py
@@ -80,7 +80,7 @@ print(start_time_points)
 # Define a range of specific time points to add/subtract
 '''Range of positive AND negative values'''
 negative_range = range(-60, 0)  # range(-10, 0) Range from -10 to -1
-positive_range = range(0, 15)    # range(0, 6) Range from 0 to 5
+positive_range = range(0, 20)    # range(0, 6) Range from 0 to 5
 
 # Merge the ranges into a single list
 time_points_range = list(negative_range) + list(positive_range)
@@ -101,18 +101,18 @@ event_time_ranges_df = pd.DataFrame(event_time_ranges, columns=['Event_Label', '
 
 # Export the DataFrame to a CSV file
 ''' Add timerange to file name '''
-event_time_ranges_df.to_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/alliged_behaviour/TL-TR-Decision-60_15TP.csv', index=False)
+event_time_ranges_df.to_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/alliged_behaviour/TL-TR-Decision-60_20TP.csv', index=False)
 
 # %%
 # Extract the data from the 'merged_activity_traces' CSV corresponding to the time points specified in the 'event_time_ranges_df' CSV. 
 
 # Read the CSV files, specifying the 'Time_Points_Range' column to be converted using ast.literal_eval
-merged_activity_traces = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/traces_3-3-1.csv')
+merged_activity_traces = pd.read_csv('/Users/nadine/Documents/paper/single-larva/ACardona_dff-file/clemclam-coordinates/measurements.csv')
 
-event_time_ranges_df = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/alliged_behaviour/truncated/TL-TR-Decision-60_15TP_Event_1-15.csv', converters={'Time_Points_Range': ast.literal_eval})
+event_time_ranges_df = pd.read_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/alliged_behaviour/TL-TR-Decision-60_20TP.csv', converters={'Time_Points_Range': ast.literal_eval})
 
 # Remove the 'timepoint' column
-merged_activity_traces.drop('time', axis=1, inplace=True)  # axis=1 specifies column-wise operation
+merged_activity_traces.drop('timepoint', axis=1, inplace=True)  # axis=1 specifies column-wise operation
 
 # Iterate through each row in event_time_ranges_df to extract data
 final_output = []
@@ -138,6 +138,6 @@ for _, row in event_time_ranges_df.iterrows():
 result_df = pd.concat(final_output, ignore_index=True)
 
 # Save the result to a new CSV file
-result_df.to_csv('/Users/nadine/Documents/paper/single-larva/generated-data/Fluorescence-traces/2023-12-05/radius_3-3-1/Action_selection/final_output_Tl-TR-Decision-60_15TP_Event_1-15.csv', index=False)
+result_df.to_csv('/Users/nadine/Documents/paper/single-larva/ACardona_dff-file/final_output_TL-TR-Decision-60_20TP-ClemClam.csv', index=False)
 
 # %%
