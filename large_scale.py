e7c8eadeadc654fd64d09556ff41e3927750ce0e
Nadine Randel <nadine@voodoo-arts.net>
2024-06-21 09:14:37 +0100

    minor

--- large_scale.py
+++ large_scale.py
@@ -10,7 +10,7 @@ import os
 #3. Find all neurons whose responses are 1.5 SD higher than the overall mean
 
 # %%
-# Important: behaviour.csv starts with "1", activity.csv with "0"
+# Important: behaviour.csv starts with "1", activity.csv with "0" - fixed for my dff
 #stimulus window 101-106/ 681-695
 
 # %%
@@ -321,5 +321,156 @@ time_range = (start_time, end_time)
 
 # Plot results without legend
 plot_neuron_activity(df, matching_neurons, time_range, 'Responsive neurons from new CSV', show_legend=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+
+# Stdev and plotting for behav
+#%%
+#1. pick a certain time window after the first stimulus onset and compute the peak DF/F and the average DF/F in that time window for each cell body
+#2. Compute the mean peak and the mean average across all cell bodies, and the SD.
+#3. Find all neurons whose responses are 1.5 SD higher than the overall mean
+
+
+# %%
+
+def plot_neuron_activity(df, neurons, time_range, title):
+    plt.figure(figsize=(12, 8))
+    for neuron in neurons:
+        plt.plot(df.index[time_range[0]:time_range[1]+1], df[neuron][time_range[0]:time_range[1]+1], label=neuron)
+    plt.title(title)
+    plt.xlabel('Timepoint')
+    plt.ylabel('ΔF/F')
+    plt.legend(loc='upper right', fontsize='small')
+    plt.grid(True)
+    plt.show()
+
+def save_neurons_to_csv(neurons, filename, path=''):  # Add a path parameter with default value ''
+    filepath = os.path.join(path, filename)  # Construct the full file path
+    pd.DataFrame(neurons).to_csv(filepath, header=False)
+
+# Load the dataset from the CSV file
+
+# dff 
+df = pd.read_csv('/Users/nadine/Documents/paper/single-larva/behavior_extraction/dff/output_dff_S_100.csv')
+
+# Drop the 'timepoint' column and use the index as the new timepoint
+df.drop(columns=['timepoint'], inplace=True)
+
+# Define the time window 
+# Forward window 51-57
+
+# Used to define neurons that correlate with behaviour 
+# Time window for statistical analysis
+# Stimulus: bhaviour-start to behviour-end
+# Time window for plotting  for statistics
+
+time_window = list(range(101, 111))
+
+# Extract the relevant rows for the time window using the index as timepoint
+time_window_data = df.iloc[time_window]
+
+# Compute the peak ΔF/F and the average ΔF/F in the time window for each neuron
+peak_dff = time_window_data.max()
+average_dff = time_window_data.mean()
+
+# Compute the mean and standard deviation for the peak and average ΔF/F across all neurons
+mean_peak_dff = peak_dff.mean()
+mean_average_dff = average_dff.mean()
+
+sd_peak_dff = peak_dff.std()
+sd_average_dff = average_dff.std()
+
+# Define the threshold for identifying responsive neurons
+#threshold_peak = mean_peak_dff + 1.5 * sd_peak_dff
+#threshold_average = mean_average_dff + 1.5 * sd_average_dff
+
+threshold_peak = mean_peak_dff + 2 * sd_peak_dff
+threshold_average = mean_average_dff + 2 * sd_average_dff
+
+# Identify neurons whose responses are 1.5 SD higher than the overall mean
+responsive_neurons_peak = peak_dff[peak_dff > threshold_peak]
+responsive_neurons_average = average_dff[average_dff > threshold_average]
+
+# Find the intersection of responsive neurons
+responsive_neurons_intersection = responsive_neurons_peak.index.intersection(responsive_neurons_average.index)
+
+# Find all neurons
+all_neurons = df.columns
+
+# Find neurons that are not responsive for both peak and average ΔF/F
+non_responsive_neurons = all_neurons.difference(responsive_neurons_intersection)
+
+# Print the results
+print("Responsive neurons based on peak ΔF/F (count: {}):".format(len(responsive_neurons_peak)))  # Number of neurons
+print(responsive_neurons_peak)
+
+print("\nResponsive neurons based on average ΔF/F (count: {}):".format(len(responsive_neurons_average)))  # Number of neurons
+print(responsive_neurons_average)
+
+# Compute the difference between responsive_neurons_peak and responsive_neurons_average
+difference_neurons = responsive_neurons_peak.index.difference(responsive_neurons_average.index)
+print("\nDifference in neurons between peak and average ΔF/F (count: {}):".format(len(difference_neurons)))
+print(difference_neurons)
+
+print("\nNeurons that are responsive based on both peak and average ΔF/F (count: {}):".format(len(responsive_neurons_intersection)))  # Number of neurons
+print(responsive_neurons_intersection)
+
+print("\nNeurons that are not responsive for both peak and average ΔF/F (count: {}):".format(len(non_responsive_neurons)))  # Number of neurons
+print(non_responsive_neurons)
+
+# Define the directory where you want to save the files
+# dff long sliding window
+#save_dir = '/Users/nadine/Documents/Zlatic_lab/Nicolo_LSM-single-cell-data/20240531_Nadine_Randel_fluorescence_measurements/WillBishop/output/dff_long/'
+
+# dff from raw-data
+save_dir = '/Users/nadine/Documents/paper/single-larva/behavior_extraction/filtered_dff'
+
+
+# Save neurons to CSV files with specified path
+save_neurons_to_csv(responsive_neurons_peak, "Forward_responsive_neurons_sd_2_peak_51-57.csv", path=save_dir)
+save_neurons_to_csv(responsive_neurons_average, "Forward_responsive_neurons_sd_2_average_51-57.csv", path=save_dir)
+#save_neurons_to_csv(difference_neurons, "difference_neurons_682-699.csv", path=save_dir)
+#save_neurons_to_csv(responsive_neurons_intersection, "responsive_neurons_intersectio_682-699.csv", path=save_dir)
+#save_neurons_to_csv(non_responsive_neurons, "non_responsive_neurons_682-699.csv", path=save_dir)
+
+# Save the original CSV file with only the columns corresponding to the responsive neurons
+df_responsive_neurons_peak = df[list(responsive_neurons_peak.index)]
+df_responsive_neurons_average = df[list(responsive_neurons_average.index)]
+#df_difference_neurons = df[list(difference_neurons)]
+#df_responsive_neurons_intersection = df[list(responsive_neurons_intersection)]
+#df_non_responsive_neurons = df[list(non_responsive_neurons)]
+
+df_responsive_neurons_peak.to_csv(os.path.join(save_dir, "Forward_activity_of_responsive_neurons_sd_2_peak_51-57.csv"), index=False)
+df_responsive_neurons_average.to_csv(os.path.join(save_dir, "Forward_activity_of_responsive_neurons_sd_2_average_51-57.csv"), index=False)
+#df_difference_neurons.to_csv(os.path.join(save_dir, "activity_of_difference_neurons_682-699.csv"), index=False)
+#df_responsive_neurons_intersection.to_csv(os.path.join(save_dir, "activity_of_responsive_neurons_intersection_682-699.csv"), index=False)
+#df_non_responsive_neurons.to_csv(os.path.join(save_dir, "activity_of_non_responsive_neurons_682-699.csv"), index=False)
+
+# Define start and end times for plotting 80-150/ 660-740
+# Note: stimulus window 101-106/ 681-695
+start_time = 80
+end_time = 150
+time_range = (start_time, end_time)
+
+# Plot results
+#plot_neuron_activity(df, responsive_neurons_peak.index, time_range, 'Responsive neurons based on peak ΔF/F')
+plot_neuron_activity(df, responsive_neurons_average.index, time_range, 'Responsive neurons based on average ΔF/F')
+#plot_neuron_activity(df, responsive_neurons_intersection, time_range, 'Neurons responsive based on both peak and average ΔF/F')
+#plot_neuron_activity(df, non_responsive_neurons, time_range, 'Neurons not responsive for both peak and average ΔF/F') #For now to many datapoints to plot
 
 # %%
--- plots/large_scale_plot.py
+++ plots/large_scale_plot.py
@@ -107,3 +107,59 @@ print(f"Columns in DataFrame:\n{df.columns}\n")
 plot_columns_in_chunks(df, chunk_size=5, x_start=x_start, x_end=x_end)
 
 # %%
+# Plot behavior-dff from raw data
+
+# for dff from raw data
+# Function to plot columns in chunks against index with specified x-axis range
+def plot_columns_in_chunks(df, chunk_size=5, x_start=None, x_end=None):
+    num_cols = df.shape[1]
+    column_chunks = [range(i, min(i + chunk_size, num_cols)) for i in range(0, num_cols, chunk_size)]
+
+    for chunk_indices in column_chunks:
+        plt.figure(figsize=(12, 8))
+        
+        plotted_anything = False  # Flag to check if anything was actually plotted
+        
+        for col_idx in chunk_indices:
+            col_name = df.columns[col_idx]
+            plt.plot(df.index, df[col_name], label=col_name)
+            plotted_anything = True  # Set flag to True if at least one plot was made
+        
+        if plotted_anything:
+            plt.title(f'Columns {chunk_indices[0]} to {chunk_indices[-1]} vs Index')
+            plt.xlabel('Index')
+            plt.ylabel('Values')
+            plt.legend()
+            plt.grid(True)
+            
+            if x_start is not None and x_end is not None:
+                plt.xlim(x_start, x_end)  # Set x-axis limits if provided
+            
+            plt.show()
+        else:
+            print(f"No data plotted for columns {chunk_indices[0]} to {chunk_indices[-1]}")
+
+# Load:
+#Stimulus 1
+#file_path = '/Users/nadine/Documents/Zlatic_lab/Nicolo_LSM-single-cell-data/20240531_Nadine_Randel_fluorescence_measurements/WillBishop/output/raw_fluorescence/F0-8_Ft-40/intersection_neuronal_activity_responsive_neurons_sd_2_average_100-110-AND-680-699_For_Stimulus_1.csv'
+
+#Stimulus 2
+file_path = '/Users/nadine/Documents/paper/single-larva/behavior_extraction/filtered_dff/Forward_activity_of_responsive_neurons_sd_2_average_51-57.csv'
+
+
+
+df = pd.read_csv(file_path, index_col = False)  # Assuming the first column is the index
+
+# Specify x-axis range (optional)
+x_start = 40  # Replace with your desired start index 80, 640
+x_end = 140   # Replace with your desired end index 140, 740
+
+# Print some debug information
+print(f"DataFrame head:\n{df.head()}\n")
+print(f"Index values:\n{df.index}\n")
+print(f"Columns in DataFrame:\n{df.columns}\n")
+
+# Plot every 20 columns against the index with specified x-axis range
+plot_columns_in_chunks(df, chunk_size=5, x_start=x_start, x_end=x_end)
+
+# %%
