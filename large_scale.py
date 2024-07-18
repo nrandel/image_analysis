3585b8ff2a3479c8c926638b3f851d40c02a4272
Nadine Randel <nadine@voodoo-arts.net>
2024-07-18 11:54:28 +0100

    fix inntersect for test

--- large_scale.py
+++ large_scale.py
@@ -8,74 +8,140 @@ import os
 
 #temp testing
 # %%
+# test for sliding window
+
 import pandas as pd
 import os
+import re
+import glob
 
-def plot_neuron_activity(df, neurons, time_range, title):
-    plt.figure(figsize=(12, 8))
-    for neuron in neurons:
-        plt.plot(df.index[time_range[0]:time_range[1]+1], df[neuron][time_range[0]:time_range[1]+1], label=neuron)
-    plt.title(title)
-    plt.xlabel('Timepoint')
-    plt.ylabel('ΔF/F')
-    plt.legend(loc='upper right', fontsize='small')
-    plt.grid(True)
-    plt.show()
+# Directory containing the stimulus files
+input_dir = '/Users/nadine/Desktop/test_3/'
 
-def save_neurons_to_csv(neurons, filename, path=''):  
-    filepath = os.path.join(path, filename)  
-    pd.DataFrame(neurons).to_csv(filepath, header=False)
+# Save directory
+save_dir = '/Users/nadine/Desktop/test_3/intersection'
+os.makedirs(save_dir, exist_ok=True)
 
-# Load the dataset from the CSV file (dff sliding window)
-df = pd.read_csv('/Users/nadine/Documents/Zlatic_lab/Nicolo_LSM-single-cell-data/20240531_Nadine_Randel_fluorescence_measurements/WillBishop/measurements_t_stacks-dff_long-slidingWindow.csv')
+# Define the action manually
+action = 'Turn'
 
-# Drop the 'timepoint' column and use the index as the new timepoint
-df.drop(columns=['timepoint'], inplace=True)
+# Use glob to list all CSV files in the directory
+file_paths = glob.glob(os.path.join(input_dir, f'{action}_*_average_*.csv'))
 
-# Define fixed window length for calculating SD (adjust as needed)
-window_length = 10
+# Check if any files are found
+if not file_paths:
+    raise FileNotFoundError(f'No files found for action "{action}". Please check the action name or the directory.')
 
-# Define start adjustment (set as needed). e.g., -9
-adjustment = 0
+# Extract timepoints from file paths
+pattern = rf'{action}_.+_average_(\d+).csv'
+actions_timepoints = [(re.search(pattern, os.path.basename(fp)).group(1), fp) for fp in file_paths]
+timepoints = sorted([(int(tp), fp) for tp, fp in actions_timepoints])
 
-# Read time windows from CSV and adjust start if needed
-time_windows_file = '/Users/nadine/Documents/paper/single-larva/behavior_extraction/action/Forward_threshold_3_18-02-15L1-behavior-ol_filtered_1-1000.csv'
-time_windows_df = pd.read_csv(time_windows_file)
+# Check if any valid files are found after pattern matching
+if not timepoints:
+    raise ValueError(f'No valid files found for action "{action}" with the specified pattern. Please check the file naming convention.')
 
-save_dir = '/Users/nadine/Documents/Zlatic_lab/Nicolo_LSM-single-cell-data/20240531_Nadine_Randel_fluorescence_measurements/WillBishop/output/dff_long/F'
 
-for index, row in time_windows_df.iterrows():
-    start_time = int(row['start'])
-    end_time = start_time + window_length
+# Function to merge DataFrames with common columns only
+def merge_dataframes(df1, df2):
+    common_columns = set(df1.columns).intersection(df2.columns)
+    merged_df = pd.merge(df1[common_columns], df2[common_columns], on=list(common_columns), how='inner')
+    return merged_df
+
+# Read the CSV files into DataFrames
+dataframes = {tp: pd.read_csv(fp) for tp, fp in timepoints}
+
+# Initialize the merged DataFrame with the first file's data
+merged_df = dataframes[timepoints[0][0]].copy()
+
+# Merge all subsequent DataFrames iteratively
+for tp, fp in timepoints[1:]:
+    current_df = dataframes[tp]
+    merged_df = merge_dataframes(merged_df, current_df)
+
+    # Update the filename
+    merged_filename = f'{action}_timepoint_' + '_'.join(map(str, [tp for tp, _ in timepoints[:timepoints.index((tp, fp))+1]])) + '.csv'
     
-    start_time_adjusted = start_time + adjustment
-    
-    time_range = (start_time_adjusted, end_time)
-    time_range_plot = (start_time_adjusted - 50, start_time_adjusted + 100)  # Adjusted for plotting
-    
-    # Compute average ΔF/F in the time window for each neuron
-    time_window_data = df.iloc[time_range[0]:time_range[1]+1]
-    average_dff = time_window_data.mean()
-    
-    # Compute mean and standard deviation for average ΔF/F
-    mean_average_dff = average_dff.mean()
-    sd_average_dff = average_dff.std()
-    
-    # Define the threshold for identifying responsive neurons
-    threshold_average = mean_average_dff + 1.5 * sd_average_dff
-    
-    # Identify neurons whose responses are 1.5 SD higher than the mean
-    responsive_neurons_average = average_dff[average_dff > threshold_average]
-    
-    # Save neurons to CSV file with specified path
-    filename = f"Forward_long-sliding-window_activity_of_responsive_neurons_sd_1-5_average_{start_time}.csv"
-    save_neurons_to_csv(responsive_neurons_average, filename, path=save_dir)
-    
-    # Save the original CSV file with only the columns corresponding to the responsive neurons
-    df_responsive_neurons_average = df[list(responsive_neurons_average.index)]
-    df_responsive_neurons_average.to_csv(os.path.join(save_dir, filename), index=False)
-    
-    # Plot results
-    plot_neuron_activity(df, responsive_neurons_average.index, time_range_plot, f'Responsive neurons based on average ΔF/F (Start: {start_time})')
+    # Save the merged DataFrame
+    merged_df.to_csv(os.path.join(save_dir, merged_filename), index=False)
 
+print('Merging and saving completed.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+"keep until intersection is fixed!!"
+#test for my dff 
+import pandas as pd
+import os
+import re
+import glob
+
+# Directory containing the stimulus files
+input_dir = '/Users/nadine/Desktop/test_1/'
+
+# Save directory
+save_dir = '/Users/nadine/Desktop/test_1/intersection'
+os.makedirs(save_dir, exist_ok=True)
+
+# Define the action manually
+action = 'Stimulus'
+
+# Use glob to list all CSV files in the directory
+file_paths = glob.glob(os.path.join(input_dir, f'{action}_*_timepoint_*.csv'))
+
+# Check if any files are found
+if not file_paths:
+    raise FileNotFoundError(f'No files found for action "{action}". Please check the action name or the directory.')
+
+# Extract timepoints from file paths
+pattern = rf'{action}_.+_timepoint_(\d+).csv'
+actions_timepoints = [(re.search(pattern, os.path.basename(fp)).group(1), fp) for fp in file_paths]
+timepoints = sorted([(int(tp), fp) for tp, fp in actions_timepoints])
+
+# Check if any valid files are found after pattern matching
+if not timepoints:
+    raise ValueError(f'No valid files found for action "{action}" with the specified pattern. Please check the file naming convention.')
+
+
+# Function to merge DataFrames with common columns only
+def merge_dataframes(df1, df2):
+    common_columns = set(df1.columns).intersection(df2.columns)
+    merged_df = pd.merge(df1[common_columns], df2[common_columns], on=list(common_columns), how='inner')
+    return merged_df
+
+# Read the CSV files into DataFrames
+dataframes = {tp: pd.read_csv(fp) for tp, fp in timepoints}
+
+# Initialize the merged DataFrame with the first file's data
+merged_df = dataframes[timepoints[0][0]].copy()
+
+# Merge all subsequent DataFrames iteratively
+for tp, fp in timepoints[1:]:
+    current_df = dataframes[tp]
+    merged_df = merge_dataframes(merged_df, current_df)
+
+    # Update the filename
+    merged_filename = f'{action}_timepoint_' + '_'.join(map(str, [tp for tp, _ in timepoints[:timepoints.index((tp, fp))+1]])) + '.csv'
+    
+    # Save the merged DataFrame
+    merged_df.to_csv(os.path.join(save_dir, merged_filename), index=False)
+
+print('Merging and saving completed.')
 # %%
